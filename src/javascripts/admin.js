2ad8b5b9a48fd67c5c58b4eb55dcb838a3e1e25a
hyeonji00 <hgkim5004@naver.com>
2022-10-12 16:51:38 +0900

    kickoff #1

---
+++ src/javascripts/admin.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const loginId = document.getElementById('LOGIN_ID');
+const loginPw = document.getElementById('LOGIN_PW');
+const loginBtn = document.getElementById('LOGIN_BTN');
+
+function color() {
+    if(loginId.value.length>0 && loginPw.value.length>=5){
+        loginBtn.style.backgroundColor = "#2E8B57";
+        loginBtn.disabled = false;
+    }
+    else{
+        loginBtn.style.backgroundColor = "#8FBC8F";
+        loginBtn.disabled = true;
+    }
+}
+
+function moveToMain(){
+    if (loginId.value == "kickoffadmin") {
+        if (loginPw.value == "aisl1234!") {
+            alert('관리자 아이디로 로그인 되었습니다.')
+            location.replace("./main.html");
+        }
+        else {
+            alert('아이디와 비밀번호를 다시 한 번 확인해주세요.')
+            errStack ++;
+        }
+    }
+    else {
+        alert('존재하지 않는 계정입니다.')
+    }
+    if (errStack >= 5) {
+        alert('비밀번호를 5회 이상 틀리셨습니다. 비밀번호 찾기를 권장드립니다.')
+    }
+}
+
+loginId.addEventListener('keyup', color);
+loginPw.addEventListener('keyup', color);
+loginBtn.addEventListener('click',moveToMain);
---
+++ src/javascripts/main.js
@@ -0,0 +1,31 @@
+var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+var options = { //지도를 생성할 때 필요한 기본 옵션
+	center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표
+	level: 1 //지도의 레벨(확대, 축소 정도)
+};
+
+var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+
+function locationLoadSuccess(pos){
+    // 현재 위치 받아오기
+    var currentPos = new kakao.maps.LatLng(pos.coords.latitude,pos.coords.longitude);
+
+    // 지도 이동(기존 위치와 가깝다면 부드럽게 이동)
+    map.panTo(currentPos);
+
+    // 마커 생성
+    var marker = new kakao.maps.Marker({
+        position: currentPos
+    });
+
+    // 기존에 마커가 있다면 제거
+    marker.setMap(null);
+    marker.setMap(map);
+};
+
+function locationLoadError(pos){
+    alert('위치 정보를 가져오는데 실패했습니다.');
+};
+
+// 위치 가져오기
+navigator.geolocation.getCurrentPosition(locationLoadSuccess,locationLoadError);
